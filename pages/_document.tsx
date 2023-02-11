9c2c4aa2f49e24f6c05b589904b633e7caa61b61
David S <david@stenwall.io>
2023-02-11 21:41:04 +0100

    chore: more project setup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,53 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import * as React from 'react';
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+// import { ServerStyleSheets } from '@mui/styles';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="sv">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;1,300;1,400&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+// MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+//   const sheets = new ServerStyleSheets();
+//   const originalRenderPage = ctx.renderPage;
+
+//   ctx.renderPage = () =>
+//     originalRenderPage({
+//       enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+//     });
+
+//   const initialProps = await Document.getInitialProps(ctx);
+
+//   return {
+//     ...initialProps,
+//     // styles fragment is rendered after the app and page rendering finish.
+//     styles: [
+//       ...React.Children.toArray(initialProps.styles),
+//       sheets.getStyleElement(),
+//     ],
+//   };
+// };
+
+export default MyDocument;
