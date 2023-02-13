ba1aede744cfa7bd41cc232ee10f2bf7ab4e4212
David S <david@stenwall.io>
2023-02-13 23:35:48 +0100

    chore: index cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,10 @@
 import useSWR from 'swr';
 import { useSession } from 'next-auth/react';
-import LogOutButton from '@components/logoutbutton/logoutbutton';
-import { Session } from 'next-auth';
 
 export default function Index() {
-  const { data, error, isLoading } = useSWR('/api/recipes');
   const { data: session } = useSession();
 
-  const recipes = data;
+  if (!session) return <div>Loading...</div>;
 
-  if (error) return <div>Failed to load</div>;
-  if (isLoading) return <div>Loading...</div>;
-
-  return (
-    <div>
-      <LogOutButton />
-      Hej {session.user.name}
-    </div>
-  );
+  return <div>Hej {session.user.name}!</div>;
 }
