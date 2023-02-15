4c7334d0d9d73fb137bffff2bbf20fa22f4e5541
David S <david@stenwall.io>
2023-02-16 00:57:46 +0100

    chore: add sort to accountRows query

--- db/queries.ts
+++ db/queries.ts
@@ -4,7 +4,7 @@ export const queries = {
   info: () => `Slantar GraphQL API`,
   account: (_, {id}) => Account.findById(id),
   accounts: () => Account.find().sort('name'),
-  accountRows: (_, {accountId}) => AccountRow.find({ account: accountId }).sort('date'),
+  accountRows: (_, {accountId}) => AccountRow.find({ account: accountId }).sort('-date text'),
   user: (_, {id}) => User.findById(id),
   users: () => User.find(),
 };
